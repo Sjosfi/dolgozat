cae0d8400072eea2cee44e397f9d8a41e61e8b0d
Sjosfi <plavi.barnus@gmail.com>
2024-10-14 13:39:34 +0200

    vege

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Render, Post } from '@nestjs/common';
+import { Controller, Get, Render, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 import { adatokDTO } from './adatokDTO.dto';
 import { Res } from '@nestjs/common';
@@ -15,15 +15,30 @@ export class AppController {
     };
   }
 
-  @Get()
+  @Get("kitolt")
   @Render('kitolt')
   getKitolt(){
 
   }
 
-  @Post()
+  @Post("kitolt")
   @Render('kitolt')
-  postKitolt(@Body adatokDTO:adatokDTO, Res() response: Response){
-
+  postKitolt(@Body adatokDTO:adatokDTO, Res, response: Response){
+    let errors = []
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!adatokDTO.email.valueOf.match(validRegex)){
+      errors.push("Hibás az email")
+    }
+    if (adatokDTO.szam > 10){
+      errors.push("Nem lehet 10-nél több személy!")
+    }
+    if (adatokDTO.szam < 1){
+      errors.push("Nem lehet 1-nél kevesebb személy!")
+    }
+    if (errors.length == 0){
+      return response.redirect("/siker")
+    }else{
+      return {nev: adatokDTO.nev, email: adatokDTO.email, idopont: adatokDTO.idopont, szam: adatokDTO.szam}
+    }
   }
 }
